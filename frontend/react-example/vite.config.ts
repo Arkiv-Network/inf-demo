687a97b989d0fcc1b3331c946edbce5e1d0955e4
Seweryn Kras <seweryn.kras@golem.network>
2025-10-31 17:26:12 +0100

    add vanilla js example to the gh pages deployment

--- frontend/react-example/vite.config.ts
+++ frontend/react-example/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/inf-demo/",
+  base: "/inf-demo/showcase/",
   plugins: [
     react({
       babel: {
