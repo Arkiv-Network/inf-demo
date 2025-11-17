e624d5a6a2d8e4f2cd21a2f2e8639dc4b87ee627
Krzysztof Fonał <krzysiekfonal@gmail.com>
2025-11-17 21:25:14 +0100

    Switch infurademo to mendoza

--- frontend/react-example/src/features/arkiv-client/constants.ts
+++ frontend/react-example/src/features/arkiv-client/constants.ts
@@ -1,20 +1,22 @@
 import type { Chain } from "@arkiv-network/sdk";
+import { mendoza } from "@arkiv-network/sdk/chains";
 
-export const ARKIV_CHAIN: Chain = {
-	id: 60138453045,
-	name: "Arkiv EthDemo Testnet",
-	nativeCurrency: {
-		decimals: 18,
-		name: "Ether",
-		symbol: "ETH",
-	},
-	rpcUrls: {
-		default: {
-			http: [`https://infurademo.hoodi.arkiv.network/rpc`],
-			webSocket: [`wss://infurademo.hoodi.arkiv.network/rpc/ws`],
-		},
-	},
-};
+export const ARKIV_CHAIN: Chain = mendoza;
+// export const ARKIV_CHAIN: Chain = {
+// 	id: 60138453045,
+// 	name: "Arkiv EthDemo Testnet",
+// 	nativeCurrency: {
+// 		decimals: 18,
+// 		name: "Ether",
+// 		symbol: "ETH",
+// 	},
+// 	rpcUrls: {
+// 		default: {
+// 			http: [`https://infurademo.hoodi.arkiv.network/rpc`],
+// 			webSocket: [`wss://infurademo.hoodi.arkiv.network/rpc/ws`],
+// 		},
+// 	},
+// };
 
 export const ENTITY_OWNER = "0xF46E23f6a6F6336D4C64D5D1c95599bF77a536f0";
 
